cb615f599ef0a12ffacef77a9a99b5a9540c8969
William Reilly <William.Reilly2010@gmail.com>
2016-04-04 22:56:17 -0400

    Can add subscription information.

--- sub_manager.rb
+++ sub_manager.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'erubis'
 require 'yaml'
+require 'money'
 
 require 'pry'
 
@@ -10,6 +11,9 @@ configure do
   enable :sessions
   set :session_secret, 'secret'
 end
+
+FREQUENCY = {1=> 'year', 2=> 'six months', 4=>'quarter', 12=>'month'}
+Money.use_i18n = false
 
 def subscription_path
   if ENV['RACK_ENV'] == 'test'
@@ -20,11 +24,52 @@ def subscription_path
 end
 
 def subscriptions_to_manage
-  YAML.load_file subscription_path
+  YAML.load_file(subscription_path) || {}
 end
 
 def update_subscriptions(subscriptions)
   File.open(subscription_path, 'w') { |f| f.write subscriptions.to_yaml}
+end
+
+def invalid_request(message='Invalid Request.')
+  session[:message] = message
+  redirect '/'
+end
+
+def subscription(sub_name)
+  sub = subscriptions_to_manage[sub_name]
+  @name = sub_name
+  @url = sub['url']
+  @frequency = FREQUENCY[sub['frequency']]
+  @cost = Money.new(sub['cost'], 'USD')
+end
+
+def sluggify(name)
+  name.downcase.gsub(/[^\s0-9A-z_]/,'').gsub(/\s/,'-')
+end
+
+def validate_cost
+  @cost =~ /^[0-9]*\.{0,1}[\d]{0,2}$/
+end
+
+def validate_subscription_params
+  @name.size > 0 &&
+  validate_cost &&
+  [1, 2, 4, 12].include?(@frequency)
+end
+
+class String
+  def to_cents
+    dollars, cents = self.split('.')
+    dollars.to_i * 100 + cents.to_i
+  end
+end
+
+def set_subscription_params
+  @name = params[:name]
+  @url = params[:url]
+  @cost = params[:cost]
+  @frequency = params[:frequency].to_i
 end
 
 get '/todo' do
@@ -41,11 +86,25 @@ get '/add' do
 end
 
 post '/add' do
-  name = params[:name]
-  subscriptions = subscriptions_to_manage || {}
-  subscriptions[name] = {}
-  update_subscriptions subscriptions
+  set_subscription_params
+  if validate_subscription_params
+    subscriptions = subscriptions_to_manage
+    subscriptions[sluggify @name] = {'name' => @name, 'cost' => @cost.to_cents, 'frequency' => @frequency.to_i, 'url' => @url}
+    update_subscriptions subscriptions
 
-  session[:message] = "#{name} has been added to your subscriptions."
-  redirect '/'
+    session[:message] = "#{@name} has been added to your subscriptions."
+    redirect '/'
+  else
+    session[:message] = "Invalid."
+    erb :add
+  end
+end
+
+get '/:subscription' do
+  if subscriptions_to_manage.key? params[:subscription]
+    subscription params[:subscription]
+    erb :subscription
+  else
+    invalid_request
+  end
 end
--- test/test_sub_manager.rb
+++ test/test_sub_manager.rb
@@ -13,9 +13,7 @@ class SubManagerTest < Minitest::Test
   end
 
   def setup
-    subscription = {'hbr' => {'website' => 'www.hbr.com', 'frequency' => 1, 'amount'=> 10000}}
     FileUtils.touch('test/subscriptions.yml')
-    File.open('test/subscriptions.yml', 'w') { |f| f.write subscription.to_yaml}
   end
 
   def teardown
@@ -26,10 +24,21 @@ class SubManagerTest < Minitest::Test
     assert_includes last_response.body, value
   end
 
+  def add_subscription(name='hbr', url='www.hbr.com', frequency='1', amount='100.00')
+
+    post '/add', {'name' => name, 'url' => url, 'frequency'=>frequency, 'cost' => amount}
+  end
+
+
+  # -------------------Tests-------------
+
   def test_index
+    add_subscription
+
     get '/'
     assert_equal 200, last_response.status
     body_includes 'Add'
+    body_includes 'hbr'
   end
 
   def test_new
@@ -38,16 +47,39 @@ class SubManagerTest < Minitest::Test
     body_includes 'Add New Subscription'
     body_includes '</form>'
     body_includes '<button type="submit">Add</button>'
+    body_includes 'frequency'
+    body_includes 'cost'
+    body_includes 'url'
 
-    post '/add', {'name' => 'Ruby Weekly'}
+    add_subscription
     assert_equal 302, last_response.status
 
     follow_redirect!
-    body_includes 'Ruby Weekly'
-    expected_message = 'Ruby Weekly has been added to your subscriptions.'
+    body_includes 'hbr'
+    expected_message = 'hbr has been added to your subscriptions.'
     body_includes expected_message
 
     get '/'
     refute_includes last_response.body, expected_message
+  end
+
+  def test_view_subscription
+    add_subscription
+    follow_redirect!
+
+    get '/hbr'
+    assert_equal 200, last_response.status
+    body_includes 'hbr'
+    body_includes 'www.hbr.com'
+    body_includes '$100.00/year'
+  end
+
+  def test_invalid_subscription
+    get '/hbr'
+    assert_equal 302, last_response.status
+
+    follow_redirect!
+    assert_equal 200, last_response.status
+    body_includes 'Invalid Request.'
   end
 end
